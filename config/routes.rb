1172636b859d7f0fc4816202df6a7267158ef095
Fahmi Ardhianto <fahmi.ardhianto@gmail.com>
2014-04-28 20:29:26 +0700

    add seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ProjectPkm::Application.routes.draw do
   resources :businesses
+  resources :categories
   root 'businesses#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
